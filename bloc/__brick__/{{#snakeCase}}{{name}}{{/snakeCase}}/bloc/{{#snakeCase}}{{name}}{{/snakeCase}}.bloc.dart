206317ca8c852e7308d4d450892be1df92e4d8a6
Vivek Brohma <vivek.brohma@gmail.com>
2024-02-13 16:13:54 +0100

    fixing initial state name

--- bloc/__brick__/{{#snakeCase}}{{name}}{{/snakeCase}}/bloc/{{#snakeCase}}{{name}}{{/snakeCase}}.bloc.dart
+++ bloc/__brick__/{{#snakeCase}}{{name}}{{/snakeCase}}/bloc/{{#snakeCase}}{{name}}{{/snakeCase}}.bloc.dart
@@ -4,6 +4,6 @@ import 'package:injectable/injectable.dart';
 @injectable
 class {{#pascalCase}}{{name}} page bloc{{/pascalCase}}
     extends Bloc<{{#pascalCase}}{{name}} page bloc event{{/pascalCase}}, {{#pascalCase}}{{name}} page bloc state{{/pascalCase}}> {
-  {{#pascalCase}}{{name}} page bloc{{/pascalCase}}() : super({{#pascalCase}}{{name}} page bloc initial{{/pascalCase}}());
+  {{#pascalCase}}{{name}} page bloc{{/pascalCase}}() : super({{#pascalCase}}{{name}} page bloc state initial{{/pascalCase}}());
 
 }
